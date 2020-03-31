c338e50faafa110f1589fb6471eb599f83d433cd
Jerard Carney <jercarne@uat.edu>
2020-03-30 19:24:16 -0600

    Tested a State Machine commit
    made and example of a stat machine

---
+++ WESS UNITY/Assets/Scripts/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] Text testTextComp;
+    [SerializeField] Test startTestText;
+
+    Test currentText; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentText = startTestText;
+        testTextComp.text = currentText.GetText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WESS UNITY/Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Test")]
+public class Test : ScriptableObject
+{
+    [TextArea(14, 10)] [SerializeField] string testText;
+
+    public string GetText()
+    {
+        return testText;
+    }
+}
+
+
